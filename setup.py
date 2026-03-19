acdd0a9107bfdee683e88f10205128af83a56f4e
dacexyai <admin.dacexy@gmail.com>
2026-03-19 19:26:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -156,13 +156,7 @@ w("src/infrastructure/persistence/database.py", """
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from src.shared.config.settings import settings
 
-engine = create_async_engine(
-    settings.async_database_url,
-    pool_size=settings.DATABASE_POOL_SIZE,
-    max_overflow=settings.DATABASE_MAX_OVERFLOW,
-    pool_pre_ping=True,
-    echo=False,
-)
+engine = create_async_engine(settings.async_database_url, pool_size=settings.DATABASE_POOL_SIZE, max_overflow=settings.DATABASE_MAX_OVERFLOW, pool_pre_ping=True, echo=False, connect_args={"ssl": "require"})
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_db():
